5dc12f964e34f9277b4e67bd66aaa674333c5c52
brygard14 <bryana.gardner@gmail.com>
2021-12-02 11:37:13 -0700

    removed env

--- server/index.js
+++ server/index.js
@@ -6,10 +6,10 @@ const path = require("path");
 
 const Rollbar = require("rollbar");
 
-const { SERVER_PORT, ROLLBAR_TOKEN } = process.env;
+// const { SERVER_PORT, ROLLBAR_TOKEN } = process.env;
 
 const rollbar = new Rollbar({
-  accessToken: ROLLBAR_TOKEN,
+  accessToken: '3d34c50745ea41a9a06c12e1769dcb2b',
   captureUncaught: true,
   captureUnhandledRejections: true,
 });
@@ -20,38 +20,37 @@ app.use(express.json());
 const students = ["alan", "sydney", "lex"];
 
 // endpoints
-app.get('/', (req, res) => {
-  rollbar.info('someone visited our site!');
+app.get("/", (req, res) => {
+  rollbar.info("someone visited our site!");
 
-  res.sendFile(path.join(__dirname, '..', '/public/index.html'));
+  res.sendFile(path.join(__dirname, "..", "/public/index.html"));
 });
 
-app.post('/api/students', (req, res) => {
-    const { name } = req.body;
+app.post("/api/students", (req, res) => {
+  const { name } = req.body;
 
-    if (!name) {
-        rollbar.error('someone tried to add an empty name!')
+  if (!name) {
+    rollbar.error("someone tried to add an empty name!");
 
-        return res.status(403).send('you must provide a name')
-    }
+    return res.status(403).send("you must provide a name");
+  }
 
-    const index = students.findIndex(studentName => name === studentName)
+  const index = students.findIndex((studentName) => name === studentName);
 
-    if (index === -1) {
-        rollbar.info('someone added a student')
+  if (index === -1) {
+    rollbar.info("someone added a student");
 
-        students.push(name);
+    students.push(name);
 
-        res.status(200).send(students);
-    } else {
-        rollbar.error('someone tried to add an existing student!')
+    res.status(200).send(students);
+  } else {
+    rollbar.error("someone tried to add an existing student!");
 
-        res.status(403).send('student already exists!')
-    }
-})
+    res.status(403).send("student already exists!");
+  }
+});
 
 rollbar.log("server started!");
 
 const port = process.env.PORT || 5050;
 app.listen(port, () => console.log(`server running on ${port}`));
-
